c5742eabbeeae2270b950a6be0bfd726627cc9e0
Menn0H <menno_hermsen@hotmail.com>
2020-10-27 12:01:49 +0100

    Removed the window.py and added all the pygame stuff into sudoku.py. Also removed the object oriented implementation and stuck with the imperative style

--- sudoku.py
+++ sudoku.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+
+import pygame
+
 boardHardU = [[0,0,8,0,0,0,0,5,0],
               [0,2,9,0,0,0,4,1,8],
               [0,0,0,0,2,0,0,0,0],
@@ -34,147 +37,214 @@ boardCom = [[3,4,6,2,1,8,9,5,7],
 	        [4,8,5,7,6,2,1,3,9]]
 
 
-class Sudoku:
-    def __init__(self, board):
-        self.board = board
+board = boardHardU
 
-    #returns in which square (1-9) the position is in
-    def getSquareNum(self, x, y):
-        x1, x2, y1, y2= [0,3,6], [2,5,8], [0,3,6], [2,5,8]
-        count = 1
+#returns in which square (1-9) the position is in
+def getSquareNum( x, y):
+    x1, x2, y1, y2= [0,3,6], [2,5,8], [0,3,6], [2,5,8]
+    count = 1
+    
+    for i in range(3):
+        for j in range(3):
+            if x >= x1[i] and y >= y1[j] and x <= x2[i] and y <= y2[j]:
+                return count
+            else:
+                count += 1
+    
+#returns an array which represents a square
+def getSquare( n):
+    count = 1
+    
+    for i in [0,3,6]:
+        for j in [0,3,6]:
+            if n == count:
+                return square(i,j)
+            else:
+                count += 1
         
-        for i in range(3):
-            for j in range(3):
-                if x >= x1[i] and y >= y1[j] and x <= x2[i] and y <= y2[j]:
-                    return count
+def square( x, y):
+    square = []
+    realy = y
+    for i in range(3):
+        for j in range(3):
+            square.append(board[x][y])	
+            y += 1
+        x += 1
+        y = realy
+
+    return square
+
+#returns if number played on position x,y is valid
+def isValidMove( x, y, num):
+    row = board[x]
+    row = [i for i in row if i != 0]
+
+    col = []
+    for i in range(9):
+        col.append(board[i][y])	
+    col = [i for i in col if i != 0]
+
+    sqr = getSquare(getSquareNum(x,y))	
+    sqr = [i for i in sqr if i != 0]
+
+    if allUnique(row) and allUnique(col) and allUnique(sqr):
+        return True
+    else:
+        return False
+
+def allUnique( n):
+    seen = set()
+    return not any(i in seen or seen.add(i) for i in n)
+
+def isValidSolution():
+    bools = []
+
+    for i in range(9):
+        for j in range(9):
+            bools.append(isValidMove(i, j, board[i][j]))
+    return all(bools)
+
+def printBoard():
+    for i in board:
+        print(i)
+
+def startingNumbers():
+    numbers = []
+
+    for i in range(9):
+        for j in range(9):
+            if board[i][j] != 0:
+                numbers.append((i, j, board[i][j]))
+    
+    return numbers
+
+def isStartingNumber( x, y, num, startNum):
+    outcome = False	
+
+    for i in startNum:
+        if i[0] == x and i[1] == y and i[2] == num:
+            outcome = True
+    
+    return outcome
+
+def solveSudoku():
+    i, j = 0, 0
+    startNum = startingNumbers()
+    number = 1
+    noValidMove = False
+
+    while i < 9:
+        while j < 9:
+            if i >= 9:
+                break
+            elif not isStartingNumber(i, j, board[i][j], startNum):
+                drawBlank(i,j)
+                if number > 9:
+                    drawBlank(i,j)
                 else:
-                    count += 1
-        
-    #returns an array which represents a square
-    def getSquare(self, n):
-        count = 1
-        
-        for i in [0,3,6]:
-            for j in [0,3,6]:
-                if n == count:
-                    return self.square(i,j)
+                    drawNumber(number, i, j, blue)
+                drawGrid(540)
+                drawIndications()
+                pygame.display.update()
+
+                if noValidMove:
+                    board[i][j] += 1
+                    number = board[i][j]
+                    noValidMove = False
                 else:
-                    count += 1
-            
-    def square(self, x, y):
-        square = []
-        realy = y
-        for i in range(3):
-            for j in range(3):
-                square.append(self.board[x][y])	
-                y += 1
-            x += 1
-            y = realy
-
-        return square
-
-    #returns if number played on position x,y is valid
-    def isValidMove(self, x, y, num):
-        row = self.board[x]
-        row = [i for i in row if i != 0]
-
-        col = []
-        for i in range(9):
-            col.append(self.board[i][y])	
-        col = [i for i in col if i != 0]
-
-        sqr = self.getSquare(self.getSquareNum(x,y))	
-        sqr = [i for i in sqr if i != 0]
-
-        if self.allUnique(row) and self.allUnique(col) and self.allUnique(sqr):
-            return True
-        else:
-            return False
-
-    def allUnique(self, n):
-        seen = set()
-        return not any(i in seen or seen.add(i) for i in n)
-
-    def isValidSolution(self):
-        bools = []
-
-        for i in range(9):
-            for j in range(9):
-                bools.append(self.isValidMove(i, j, self.board[i][j]))
-        return all(bools)
-
-    def printBoard(self):
-        for i in self.board:
-            print(i)
-
-    def startingNumbers(self):
-        numbers = []
-
-        for i in range(9):
-            for j in range(9):
-                if self.board[i][j] != 0:
-                    numbers.append((i, j, self.board[i][j]))
-        
-        return numbers
-
-    def isStartingNumber(self, x, y, num, startNum):
-        outcome = False	
-
-        for i in startNum:
-            if i[0] == x and i[1] == y and i[2] == num:
-                outcome = True
-        
-        return outcome
-
-    def solveSudoku(self):
-        i, j = 0, 0
-        startNum = self.startingNumbers()
-        number = 1
-        noValidMove = False
-
-        while i < 9:
-            while j < 9:
-                if i >= 9:
-                    break
-                elif not self.isStartingNumber(i, j, self.board[i][j], startNum):
-                    if noValidMove:
-                        self.board[i][j] += 1
-                        number = self.board[i][j]
-                        noValidMove = False
-                    else:
-                        self.board[i][j] = number
-                        if number > 9:
-                            self.board[i][j] = 0
-                            j -= 1
-                            if j < 0:
-                                j = 8
-                                i -= 1
-                            noValidMove = True
-                            number = 1
-                        elif self.isValidMove(i, j, number):
-                            j += 1
-                            if j == 9:
-                                j = 0
-                                i += 1
-                            number = 1
-                        else:
-                            number += 1
-                else:
-                    if noValidMove:
+                    board[i][j] = number
+                    if number > 9:
+                        board[i][j] = 0
                         j -= 1
                         if j < 0:
                             j = 8
                             i -= 1
-                    else:
+                        noValidMove = True
+                        number = 1
+                    elif isValidMove(i, j, number):
                         j += 1
-                        if j > 8:
+                        if j == 9:
                             j = 0
                             i += 1
+                        number = 1
+                    else:
+                        number += 1
+            else:
+                if noValidMove:
+                    j -= 1
+                    if j < 0:
+                        j = 8
+                        i -= 1
+                else:
+                    j += 1
+                    if j > 8:
+                        j = 0
+                        i += 1
 
-if __name__ == '__main__':
-    s = Sudoku(boardUnc)
-    s.printBoard()
-    print('\n')
 
-    s.solveSudoku()
-    s.printBoard()
+white = pygame.Color('white')
+black = pygame.Color('black')
+blue = pygame.Color('blue')
+
+running = True
+width, height = 1000, 800
+
+pygame.init()
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Sudoku Solver")
+
+def drawGrid(width):
+    squareSize = width // 9
+
+    for i in range(20, (width + 20), squareSize):
+        for j in range(20, (width + 20), squareSize):
+            pygame.draw.rect(screen, black, (i,j,squareSize,squareSize), 3)
+
+def drawNumber(number, x, y, colour):
+    font = pygame.font.Font('freesansbold.ttf', 58)
+    text = font.render(str(number), True, colour)
+    textRect = text.get_rect()
+    textRect.center = ((50 + y * 60), (55 + x * 60))
+    screen.blit(text, textRect)
+
+def drawBlank(x, y):
+    squareSize = width // 9
+    blank = pygame.Rect(0,0,(squareSize-50), (squareSize-50))
+    blank.center = ((50 + y * 60), (55 + x * 60))
+    pygame.draw.rect(screen, white, blank,0)
+
+def drawStartingNumbers():
+    number = startingNumbers()
+    
+    for i in number:
+        drawNumber(i[2], i[0], i[1], black)
+
+def drawIndications():
+    pygame.draw.line(screen, blue, (20,200), (560,200), 6)
+    pygame.draw.line(screen, blue, (20,380), (560,380), 6)
+    pygame.draw.line(screen, blue, (200,20), (200,560), 6)
+    pygame.draw.line(screen, blue, (380,20), (380,560), 6)
+
+def setup():
+    screen.fill(white)
+
+    drawGrid(540)
+    drawIndications()
+    drawStartingNumbers()
+    pygame.display.update()
+
+def event(event):
+    if event.type == pygame.QUIT:
+        running = False
+
+def execute():
+    setup()
+
+    solveSudoku()
+
+    while running:
+        for e in pygame.event.get():
+            event(e)
+
+    
+execute()
